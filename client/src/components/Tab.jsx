26bcd0d1f60002fef2e04d9bb8aa71d19d66067d
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2023-12-05 16:41:41 -0600

    avance 4

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import {useSnapshot} from 'valtio'
+
+import state from '../store'
 
 function Tab() {
+  const snap = useSnapshot(state)	
   return (
     <div>Tab</div>
   )
