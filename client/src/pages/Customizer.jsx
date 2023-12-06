b729553664e4b1c54b511be148c0339c198990c8
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2023-12-05 22:18:08 -0600

    Update Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -49,7 +49,7 @@ function Customizer() {
     try {
       setGeneratingImg(true)
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle',{
+      const response = await fetch('https://project-threejs-ai-54m2.onrender.com/api/v1/dalle',{
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
