83e1675c9a79b52f7f8ddb9d0ae7c920aeced961
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2023-12-05 21:11:40 -0600

    server

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -33,9 +33,27 @@ function Customizer() {
         readFile={readFile}
         />
       case 'aipicker':
-        return<AIPicker/>
+        return<AIPicker
+        prompt={prompt}
+        setPrompt={setPrompt}
+        generatingImg={generatingImg}
+        handleSubmit={handleSubmit}
+        />
         default: 
         return null
+    }
+  }
+
+  const handleSubmit = async(type)  => {
+    if(!prompt) return alert('Please enter a prompt')
+    try {
+      // call our backend to generate an image
+    } catch (error) {
+      alert(error)
+    }
+    finally{
+      setGeneratingImg(false)
+      setActiveEditorTab('')
     }
   }
 
